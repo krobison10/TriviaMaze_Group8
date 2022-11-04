61a65fff5b43a8b41309eaec3e17f0f8e9ee11e8
krobison10 <kylerrobison10@outlook.com>
2022-11-04 15:02:06 -0700

    Boilerplate for Door class, documentation of current fields.

--- src/main/java/model/Door.java
+++ src/main/java/model/Door.java
@@ -1,5 +1,42 @@
 package model;
 
 public class Door {
+    /**
+     * Represents the question that the door contains
+     */
+    private Question myQuestion;
+    /**
+     * Represents the state of the door. A door can be closed, meaning that
+     * the user has not yet correctly answered the question, nor incorrectly, and
+     * therefore is not permitted to pass through yet. It can be opened if the user
+     * answered the question correctly, allowing the player to pass through. Or it can be
+     * blocked, meaning that the question was answered incorrectly and the door
+     * will be un-passable for the remainder of the game.
+     */
+    private DoorStates myState;
+
+
+    public Door(final Question theQuestion) {
+        myQuestion = theQuestion;
+    }
+
+    public Question getQuestion() {
+        return myQuestion;
+    }
+
+    private Question ChangeQuestion(final Question theQuestion) {
+        myQuestion = theQuestion;
+        return myQuestion;
+    }
+
+    public DoorStates getState() {
+        return myState;
+    }
+
+    public DoorStates setState(final DoorStates theState) {
+        myState = theState;
+        return myState;
+    }
+
 
 }
