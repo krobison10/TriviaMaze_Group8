530c761b138e5057faedb4e4cc796aec828e71a6
krobison10 <kylerrobison10@outlook.com>
2022-11-06 12:06:19 -0800

    Boilerplate for TriviaMazeInitializer and documentation of fields

--- src/main/java/controller/TriviaMazeInitializer.java
+++ src/main/java/controller/TriviaMazeInitializer.java
@@ -1,4 +1,35 @@
 package controller;
 
+import model.TriviaMaze;
+import view.TriviaMazeUI;
+
 public class TriviaMazeInitializer {
+    /**
+     * Reference to the main class of the model package
+     */
+    private TriviaMaze myMaze;
+    /**
+     * Reference to the main class of the view package
+     */
+    private TriviaMazeUI myUI;
+
+    TriviaMazeInitializer() {
+
+    }
+
+    private TriviaMaze initializeMaze() {
+        return null;
+    }
+
+    private TriviaMazeUI initializeUI() {
+        return null;
+    }
+
+    TriviaMaze getMaze() {
+        return myMaze;
+    }
+
+    TriviaMazeUI getUI() {
+        return myUI;
+    }
 }
