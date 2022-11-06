59bdaaba85fc2117cc61d2edca85af9f1188f4d5
krobison10 <kylerrobison10@outlook.com>
2022-11-06 12:03:07 -0800

    Boilerplate for TriviaMazeController and documentation of fields

--- src/main/java/controller/TriviaMazeController.java
+++ src/main/java/controller/TriviaMazeController.java
@@ -1,5 +1,31 @@
 package controller;
 
-public class TriviaMazeController {
+import model.TriviaMaze;
+import view.TriviaMazeUI;
 
+public class TriviaMazeController {
+    /**
+     * Reference to the main class of the model package
+     */
+    private TriviaMaze myMaze;
+    /**
+     * Reference to the main class of the view package
+     */
+    private TriviaMazeUI myUI;
+
+    public TriviaMazeController() {
+
+    }
+
+    private void initialize() {
+
+    }
+
+    private boolean gameLoop() {
+        return false;
+    }
+
+    private boolean promptQuestion() {
+        return false;
+    }
 }
