d46f0082239d0b305d77a5c881f81df37611522c
AJ Garcia <amg@hostOne-2.local>
2022-11-07 16:16:43 -0800

    added jframe call from main and menu listeners

--- src/main/java/driver/Main.java
+++ src/main/java/driver/Main.java
@@ -1,7 +1,13 @@
 package driver;
 
+import view.BuildGUI;
+
 public class Main {
     public static void main(String[] args) {
+
+        BuildGUI gui = new BuildGUI();
+        gui.buildFrame();
+        
 
     }
 }
