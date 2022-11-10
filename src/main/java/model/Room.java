09c2c9b1114ab6bf8e9e77b20e7793c881002e41
krobison10 <kylerrobison10@outlook.com>
2022-11-10 13:26:03 -0800

    Tweaks to Room and documentation.

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Represents a room in the maze. Contains functionality to initialize itself, specifically about
+ * creating doors correctly and handling sharing doors with adjacent rooms. Stores data about its
+ * doors and also the item that the room may contain.
+ */
 public class Room {
     /**
      * Represents the position of the room within the maze
@@ -22,6 +27,10 @@ public class Room {
      * then the reference will be null.
      */
     private List<Door> myDoors;
+    /**
+     * Reference to the containing maze.
+     */
+    private final TriviaMaze myMaze;
 
     /**
      * Constructs a room object and handles necessary operations such
@@ -39,6 +48,8 @@ public class Room {
         myPosX = theX;
         myPosY = theY;
 
+        myMaze = theMaze;
+
         //Makes sure that myDoors isn't null to avoid null references when initializeDoors is called
         Door[] temp = {null, null, null, null};
         myDoors = new ArrayList<>(Arrays.asList(temp));
@@ -52,35 +63,10 @@ public class Room {
     }
 
     /**
-     * Removes the item from the room.
-     * @return the removed Item.
-     */
-    public Item takeItem() {
-        var temp = myItem;
-        myItem = null;
-        return temp;
-    }
-
-    /**
      * @return the Item that the room contains.
      */
     public Item getItem() {
         return myItem;
-    }
-
-    /**
-     *
-     * @param theItem the Item to be added to the room.
-     * @return true if the item was added successfully, false if the room already contained an item.
-     * In the false case, the existing item will be kept and the item to be added will be ignored.
-     */
-    public boolean addItem(final Item theItem) {
-        boolean successful = false;
-        if(myItem == null) {
-            myItem = theItem;
-            successful = true;
-        }
-        return successful;
     }
 
     /**
@@ -107,42 +93,65 @@ public class Room {
     }
 
     /**
-     * Calls setDoors to initialize the doors for the room.
-     * @param theMaze a reference to the containing maze object.
+     *
+     * @param theItem the Item to be added to the room.
+     * @return true if the item was added successfully, false if the room already contained an item.
+     * In the false case, the existing item will be kept and the item to be added will be ignored.
      */
-    void initializeDoors(final TriviaMaze theMaze) {
-        myDoors = setDoors(theMaze);
+    boolean addItem(final Item theItem) {
+        boolean successful = false;
+        if(myItem == null) {
+            myItem = theItem;
+            successful = true;
+        }
+        return successful;
+    }
+
+    /**
+     * Removes the item from the room.
+     * @return the removed Item.
+     */
+    Item takeItem() {
+        var temp = myItem;
+        myItem = null;
+        return temp;
+    }
+
+    /**
+     * Calls setDoors to initialize the doors for the room.
+     */
+    void initializeDoors() {
+        myDoors = setDoors();
     }
 
     /**
      * Loops through all the directions, for each direction, if in bounds
      * it either gets a reference to a door object from the adjacent room
      * to use for this room, or creates a new one if there isn't one yet.
-     * @param theMaze a reference to the TriviaMaze.
      * @return a List of doors for the room.
      */
-    private List<Door> setDoors(final TriviaMaze theMaze) {
+    private List<Door> setDoors() {
         Door[] init = {null, null, null, null};
         List<Door> result = Arrays.asList(init);
         if(myPosX - 1 >= 0) {
             //Get door from room to the west that current room would share
-            var adjacent = theMaze.getRoom(myPosX - 1, myPosY).getDoors().get(2);
-            result.set(0, (adjacent == null ? theMaze.addDoor(new Door()) : adjacent));
+            var adjacent = myMaze.getRoom(myPosX - 1, myPosY).getDoors().get(2);
+            result.set(0, (adjacent == null ? myMaze.addDoor(new Door(myMaze)) : adjacent));
         }
         if(myPosY - 1 >= 0) {
             //Get door from room to the north that current room would share
-            var adjacent = theMaze.getRoom(myPosX, myPosY - 1).getDoors().get(3);
-            result.set(1, (adjacent == null ? theMaze.addDoor(new Door()) : adjacent));
+            var adjacent = myMaze.getRoom(myPosX, myPosY - 1).getDoors().get(3);
+            result.set(1, (adjacent == null ? myMaze.addDoor(new Door(myMaze)) : adjacent));
         }
-        if(myPosX + 1 < theMaze.getWidth()) {
+        if(myPosX + 1 < myMaze.getWidth()) {
             //Get door from room to the east that current room would share
-            var adjacent = theMaze.getRoom(myPosX + 1, myPosY).getDoors().get(0);
-            result.set(2, (adjacent == null ? theMaze.addDoor(new Door()) : adjacent));
+            var adjacent = myMaze.getRoom(myPosX + 1, myPosY).getDoors().get(0);
+            result.set(2, (adjacent == null ? myMaze.addDoor(new Door(myMaze)) : adjacent));
         }
-        if(myPosY + 1 < theMaze.getHeight()) {
+        if(myPosY + 1 < myMaze.getHeight()) {
             //Get door from room to the north that current room would share
-            var adjacent = theMaze.getRoom(myPosX, myPosY + 1).getDoors().get(1);
-            result.set(3, (adjacent == null ? theMaze.addDoor(new Door()) : adjacent));
+            var adjacent = myMaze.getRoom(myPosX, myPosY + 1).getDoors().get(1);
+            result.set(3, (adjacent == null ? myMaze.addDoor(new Door(myMaze)) : adjacent));
         }
         return result;
     }
