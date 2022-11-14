ccbfe8dd5fb86763d0bc6ebea75242437d682894
krobison10 <kylerrobison10@outlook.com>
2022-11-14 15:32:02 -0800

    Unit tests for question bank.

--- src/test/java/model/QuestionBankTest.java
+++ src/test/java/model/QuestionBankTest.java
@@ -1,43 +1,59 @@
 package model;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
-import java.sql.ResultSet;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuestionBankTest {
 
-    static Method processEntry, initializeQuestions;
+    static Method initializeQuestions;
     static QuestionBank qb;
 
     @BeforeAll
     static void init() throws NoSuchMethodException {
-        qb = new QuestionBank("questions.db");
+        qb = new QuestionBank("test_questions.db");
 
         //Reflection to access private methods
         initializeQuestions = qb.getClass().getDeclaredMethod("initializeQuestions", String.class);
         initializeQuestions.setAccessible(true);
 
-        processEntry = qb.getClass().getDeclaredMethod("processEntry", ResultSet.class);
-        processEntry.setAccessible(true);
-
     }
-
     @Test
+    @DisplayName("QuestionBank: read question objects from database")
+    void initializeQuestions() throws Exception {
+        List<Question> questions = (List<Question>) initializeQuestions.invoke(qb, "test_questions.db");
+        assertEquals(3, questions.size());
+    }
+    @Test
+    @DisplayName("QuestionBank: add question")
     void addQuestion() {
-
+        qb = new QuestionBank("test_questions.db");
+        int size = qb.size();
+        qb.addQuestion(new QuestionFA("", ""));
+        assertEquals(size + 1, qb.size());
     }
 
     @Test
+    @DisplayName("QuestionBank: remove question")
     void removeQuestion() {
-
+        qb = new QuestionBank("test_questions.db");
+        int size = qb.size();
+        Question q = qb.getQuestion(0);
+        qb.removeQuestion(q);
+        assertEquals(size - 1, qb.size());
     }
 
     @Test
-    void getQuestion() {
-
+    @DisplayName("QuestionBank: remove random question")
+    void getRandomQuestion() {
+        qb = new QuestionBank("test_questions.db");
+        int size = qb.size();
+        qb.getRandomQuestion();
+        assertEquals(size - 1, qb.size());
     }
 }
