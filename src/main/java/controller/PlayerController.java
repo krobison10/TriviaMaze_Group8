002f89fc6cf3a4e23b3e4bc2eb95ca160c582bc7
krobison10 <kylerrobison10@outlook.com>
2022-11-20 20:22:42 -0800

    Few tweaks

---
+++ src/main/java/controller/PlayerController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import model.mazeElements.Player;
+
+public class PlayerController {
+
+    private final Player myPlayer;
+
+    public PlayerController(final Player thePlayer) {
+        myPlayer = thePlayer;
+
+    }
+}
