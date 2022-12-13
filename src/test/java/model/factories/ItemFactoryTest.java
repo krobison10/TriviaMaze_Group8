15c331f6bef7b369451aa2e03c719d126dee1a1d
Minh Le <minhlecs@icloud.com>
2022-12-12 21:12:22 -0800

    Disable True False hint

--- src/test/java/model/factories/ItemFactoryTest.java
+++ src/test/java/model/factories/ItemFactoryTest.java
@@ -5,17 +5,33 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+/**
+ * Unit Test for ItemFactory class
+ *
+ * @author Minh Le
+ */
 class ItemFactoryTest {
 
+    /**
+     * Ruler item
+     */
     private static Item ruler;
+    /**
+     * Eraser item
+     */
     private static Item eraser;
+    /**
+     * Pencil item
+     */
     private static Item pencil;
 
     @AfterAll
     static void tearDownAfterClass() throws Exception {
     }
 
+    /**
+     * Unit test for Creating item to change question
+     */
     @Test()
     @DisplayName("Create item to change question")
     void testCreateQuestionChangeItem() {
@@ -26,6 +42,9 @@ class ItemFactoryTest {
                 "Item ruler should have type change question.");
     }
 
+    /**
+     * Unit test for Creating item to remove 2 wrong options
+     */
     @Test()
     @DisplayName("Create item to remove 2 wrong options")
     void testCreateItemToRemoveWrongOptions() {
@@ -36,6 +55,9 @@ class ItemFactoryTest {
                 "Item eraser should have type option removal.");
     }
 
+    /**
+     * Unit test for Creating item to display hints for free answer question type
+     */
     @Test()
     @DisplayName("Create item to display hints for free answer question type")
     void testCreateItemToDisplayHints() {
@@ -46,6 +68,9 @@ class ItemFactoryTest {
                 "Item pencil should have type hint display.");
     }
 
+    /**
+     * Unit test for Creating item with wrong type throw exception
+     */
     @Test()
     @DisplayName("Create item with wrong type throw exception")
     void testCreateItemWithWrongType() {
